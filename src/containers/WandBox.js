ad793c031bfe43482a70d8f003b5a7f01f129ec1
Simon Atkins <this-is-simon@github.com>
2018-06-12 14:03:10 +0100

    Created initial files

---
+++ src/containers/WandBox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CharacterSelector from '../components/CharacterSelector';
+import WandSelector from '../components/WandSelector';
+
+class WandSelector extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      characters = [];
+      currentCharacter = '';
+    }
+  }
+
+  render(){
+    return
+  }
+
+
+
+
+}
+
+export default WandBox;
