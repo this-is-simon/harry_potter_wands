0e6931588c19fdc0f4c211174276bae96e4ce125
Simon Atkins <this-is-simon@github.com>
2018-06-12 14:39:02 +0100

    Added selector and getting data from API

---
+++ src/components/CharacterSelector.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CharacterSelector = (props) => {
+
+
+
+  return(
+    <select className="character-select">
+      <option disabled value="default">Choose a Student...</option>
+    </select>
+  )
+}
+
+export default CharacterSelector;
--- src/containers/WandBox.js
+++ src/containers/WandBox.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import CharacterSelector from '../components/CharacterSelector';
+import CharacterSelector from '../components/CharacterSelector.js';
 // import WandSelector from '../components/WandSelector';
 
 class WandBox extends React.Component{
+
   constructor(props){
     super(props);
     this.state = {
       characters: [],
-      currentCharacter: ''
+      currentCharacter: null
     }
   }
+
+  componentDidMount(){
+    const url = 'http://hp-api.herokuapp.com/api/characters/students';
+    fetch(url)
+    .then((response) =>  response.json())
+    .then(charactersData => this.setState({characters: charactersData}))
+    .catch(error => console.log("Error:", error))
+  }
+
 
   render(){
     return(
       <div className="wand-box">
-        THIS IS THE WANDBOX
+        <CharacterSelector />
       </div>
     )
   }
-
-
-
 
 }
 
